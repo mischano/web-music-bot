996455f8d1af96f891fc7f38bd1c2c06b5c78621
mischano <mansur.ischanov@gmail.com>
2023-06-26 11:52:25 -0700

    Failed to fix non-utf8 encodings.

--- public/scripts/ytdlp.py
+++ public/scripts/ytdlp.py
@@ -6,11 +6,14 @@ requestedAudio = ' '.join(sys.argv[1:])
 ydl_opts = {
     'format': 'bestaudio/best',
     'quiet': True,
+    'noplaylist': True,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
+    # 'restrict-filenames': True,
+    # 'restritfilenames': True,
 }
 
 # res = {
@@ -18,13 +21,11 @@ ydl_opts = {
 #     'AUDIO_URL': None,
 # }
 res = ""
-
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     try:
         info = ydl.extract_info("ytsearch:%s" % requestedAudio, download=False)['entries'][0]
     except yt_dlp.utils.DownloadError or yt_dlp.utils.ExtractorError:
         print("BIG FAIL")
-
 res = res + "AUDIO_TITLE: " + info['title']
 res = res + " "
 res = res + "AUDIO_URL: " + info['formats'][3]['url']
@@ -32,3 +33,4 @@ res = res + "AUDIO_URL: " + info['formats'][3]['url']
 # res['AUDIO_TITLE'] = info['title']
 
 print(res)
+
