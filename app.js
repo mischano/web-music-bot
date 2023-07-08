c74ced311a3e6f0e62f4c8aa7bfbf770510773d5
mischano <mansur.ischanov@gmail.com>
2023-07-08 08:59:37 -0700

    Debugging.

--- app.js
+++ app.js
@@ -29,11 +29,26 @@ app.use('/', require('./routes/index'));    // Home page.
 app.use('/users', require('./routes/users'));   // To be implemented.
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'css');
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+const allowedOrigins = ['www.googlevideo.com',
+                      'googlevideo.com',
+                      'https://manifest.googlevideo.com'];
+app.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  
+  }));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 const PORT = process.env.PORT || 5000;
 
