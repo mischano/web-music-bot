50f56c81d54266fc69113882af1cb9e846cc1afc
mischano <mansur.ischanov@gmail.com>
2023-07-12 18:57:26 -0700

    Cleaned up .py script.

--- public/scripts/py/ytdlp.py
+++ public/scripts/py/ytdlp.py
@@ -3,39 +3,28 @@ import json
 import yt_dlp
 
 
-# def format_selector(ctx):
-#     """ Select the best video and the best audio that won't result in an mkv.
-#     NOTE: This is just an example and does not handle all cases """
+def format_selector(ctx):
+    formats = ctx.get('formats')[::-1]
 
-#     # formats are already sorted worst to best
-#     formats = ctx.get('formats')[::-1]
+    best_video = next(f for f in formats
+                      if f['vcodec'] != 'none' and f['acodec'] == 'none')
 
-#     # acodec='none' means there is no audio
-#     best_video = next(f for f in formats
-#                       if f['vcodec'] != 'none' and f['acodec'] == 'none')
+    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+    best_audio = next(f for f in formats if (
+        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
 
-#     # find compatible audio extension
-#     # audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
-#     audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
-#     # vcodec='none' means there is no video
-#     best_audio = next(f for f in formats if (
-#         f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
-
-#     # These are the minimum required fields for a merged format
-#     yield {
-#         'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
-#         'ext': best_video['ext'],
-#         'requested_formats': [best_video, best_audio],
-#         # Must be + separated list of protocols
-#         'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
-#     }
+    yield {
+        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
+        'ext': best_video['ext'],
+        'requested_formats': [best_video, best_audio],
+        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
+    }
 
 
 requestedAudio = ' '.join(sys.argv[1:])
 
 ydl_opts = {
     'format': '(bestaudio/best)[protocol~="^https?$"]',
-    'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
     'quiet': True,
     'noplaylist': True,
     'postprocessors': [{
@@ -43,6 +32,7 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
+    'extractor_args': {'youtube':{'player_client': ['android', 'web']}},
 }
 
 obj = {
